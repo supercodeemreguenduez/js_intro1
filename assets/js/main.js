1843395fa6e8c31bff91ec1e384a8f47194710cd
supercodemereguenduez <supercode.emreguenduez@gmail.com>
2022-04-20 14:01:59 +0200

    ready

---
+++ assets/js/main.js
@@ -0,0 +1,160 @@
+// LEV1_1
+// // function myFunction(
+// //     document.getElementById("test").style.color = "red");
+
+// LEV1_2
+
+// // // This is a single line JavaScript comment
+// // document.write("I have comments in my JavaScript code!");
+// // //document.write("You can't see this!");
+// // //-->
+
+
+// // document.write("I have multi-line comments!");
+// // /*document.write("You can't see this!");
+// // document.write("You can't see this!");
+// // document.write("You can't see this!");
+// // document.write("You can't see this!");
+// // document.write("You can't see this!");
+// // document.write("You can't see this!");
+// // document.write("You can't see this!");*/
+// // // 
+
+// LEV1_7
+
+// let helloWorld = "Hello World!";
+// const PI = 3.141592653589793;
+// const burjDubai = "828m";
+// let fullName = "Jan Schmidt";
+// const eifelTower = "324m";
+// let camelCase = camelCase;
+// let kursStatus = true;
+
+// LEV1_1_CONSOLE
+
+// a = 12;
+// b = a * 3;
+// console.log(b);
+// document.write(b);
+
+// console.log(document.URL);
+// console.log("Hello World!");
+
+// console.log("Anton");
+
+// let name = "Anton";
+// console.log(name);
+
+// let age = 28;
+// console.log(age);
+
+// let job = "Trainer";
+// console.log(job);
+
+// let married = true;
+// console.log(married);
+
+// console.log(name + ' ist ' + age + ' Jahre alt, von Beruf ' + job + ' und ist verheiratet: ' + married);
+
+
+// LEV1_5_JS_VARIABLEN
+
+// let carName = "BMW";
+// console.log(carName);
+
+// let x = 150;
+// console.log(x);
+
+// let y = 50;
+// console.log(y);
+
+// let z = x + y;
+// console.log(z);
+
+// let firstName = "John"; 
+// let lastName = "Doe"; 
+// let age = 35;
+// console.log(firstName, lastName, age);
+
+
+
+// LEV1_6_JS_VARIABLEN
+// let z = 34;
+// z = 67;
+// console.log(z);
+
+// let x = 34;
+// x = 67;
+// console.log(x);
+
+// const y = 34;
+// y = 67;
+// console.log(y);
+
+
+
+// Lev1_12_js-einführung_arithmetic_operators
+
+// let score = 5 + 5 * 10;
+// console.log('Ergebnis: ' + score);
+
+// let score1 = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score1);
+
+// let score2 = 0;
+// score2 = score2 + 10;
+// console.log('Ergebnis: ' + score2);
+
+// score2 += 10;
+// console.log('Ergebnis: ' + score2);
+
+// let zahl = 1;
+// zahl = zahl + 1;
+// zahl += 1;
+// zahl++;
+// console.log('increment: ' + zahl);
+// zahl--;
+// console.log('dekrement: ' + zahl);
+
+
+// Lev1_13_js-einführung_arithmetische_wiederholung
+
+// // Addition
+// let addition_operator = 1 + 1;
+// console.log("addition: " + addition_operator);
+
+// // Subtraktion
+// let subtraktion_operator = 2 - 1;
+// console.log("subtraktion: " + subtraktion_operator);
+
+// // Multiplikation
+// let multiplication_operator = 2 * 2;
+// console.log("multiplication: " + multiplication_operator);
+
+// // Division
+// let division_operator = 4 / 2;
+// console.log("division: " + division_operator);
+
+// // Modul: zeigt an, was der Rest sein würde.
+// let modulus_operator = 14 % 5;
+// console.log("modulus: " + modulus_operator);
+
+
+// Lev1_11_js-einführung_string-methoden
+
+// let x = "Hello "
+// let y = "World<br>"
+// let z = x + y;
+
+// document.write(z);
+
+// let n = x + " " + y;
+// document.write(n);
+
+// x = x + "World";
+// document.write(x);
+
+// let meinString = "Ich bin Erster.";
+// let a = "Ich komme auf Platz zwei.";
+// meinString += a;
+// console.log(meinString);
